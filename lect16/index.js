e3cd11fef16cb4a37a614e64fc5daee725195080
PriyalPatel30 <priyal30patel@gmail.com>
2025-01-11 19:43:19 +0530

    JS

---
+++ lect16/index.js
@@ -0,0 +1,46 @@
+// console.log("hello");
+
+// Number
+// a=5.5
+// String
+// Boolean true false 0,1
+// null
+// undefined
+// Symbol
+// BigInt
+// int a
+
+// var ,late,const
+// var a=5
+// console.log(a)
+// var a=5
+// var a=10
+// console.log(a);
+
+
+// let a=5
+// a
+// console.log(a);
+
+// const c=100
+// c=1000
+// console.log(c);
+
+// console.log(0.1+0.2==0.3);
+
+// var a=100
+// var to=a==100
+// console.log(to);
+
+
+// let a='vicky'
+// let b='jain'
+//  console.log(`${a}     ${b}`);
+
+// = assignment operator
+// == checks only value , not the data type
+// === checks value as well as data type
+
+// console.log();  // prints nothing or empty
+
+
