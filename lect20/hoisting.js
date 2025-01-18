bf89dc48c9450a5bbc3e3ae6bf3e12eaf1aecbca
PriyalPatel30 <priyal30patel@gmail.com>
2025-01-18 19:47:17 +0530

    Hoisting

---
+++ lect20/hoisting.js
@@ -0,0 +1,30 @@
+// Hoisting
+        // console.log(a);
+        // const a=5
+
+        // var a
+        // console.log(a);
+        // a=5
+
+        // outer()
+        // function outer(){
+        //     console.log('hello');
+            
+        // }
+
+        // sum()
+        // var sum()
+        
+        // console.log(a());
+        // sum()
+        // function sum(){
+        //     console.log('helllooo');
+            
+        // }
+        
+
+        outer()
+        var outer=()=>{
+            console.log('hiiii');
+            
+        }
