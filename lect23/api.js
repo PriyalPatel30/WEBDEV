9b9778b04653fc6ab8fc0af8f4009e31caa75e22
PriyalPatel30 <priyal30patel@gmail.com>
2025-01-25 19:48:49 +0530

    API and DOM

---
+++ lect23/api.js
@@ -0,0 +1,42 @@
+// API = APPLICATION INTERFACE PROGRAMMING ,CONNECTS FRONTEND AND BACKEND
+
+// let apiLink='https://dummyjson.com/recipes'
+// let data = fetch(apiLink)
+
+// // console.log(data,'sksuh');   
+
+// data.then((res)=> {
+//     // console.log(res,'hndkslsjslb');
+    
+//     return res.json()
+// }).then((d)=>{
+//     // console.log(d.recipes[0].name,'owowowowowo');               // This gives the name from the 0th index of the array of recipes
+
+    
+//     let filteredData = d.recipes.filter((apiData)=>{
+//         // console.log(apiData);                                   // apiData is the variable here and gives the entire data of the api
+
+//         return apiData.cuisine == 'Indian'
+//     })
+
+//     console.log(filteredData,'wht are u');
+    
+// })
+
+
+// DOM  html+js
+
+// console.log(hello); d o m
+// console.log(document);
+
+let data = document.getElementById('one')
+console.log(data,'lkooi');
+
+// let data = document.getElementsByClassName('two')
+
+// for(let i of data){
+//     i.style.color='red'
+// }
+ 
+// data.style.color='red'
+
