1c8b44c0a92a12908806526eb21ff55d3134b001
PriyalPatel30 <priyal30patel@gmail.com>
2025-01-27 19:49:29 +0530

    QuerySelectors and ElementCreation with js

---
+++ Assignments/assignment14.js
@@ -0,0 +1,16 @@
+
+const arr = [
+    'https://images.unsplash.com/photo-1560343776-97e7d202ff0e?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1501432377862-3d0432b87a14?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+     'https://images.unsplash.com/photo-1626278664285-f796b9ee7806?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1608889175250-c3b0c1667d3a?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    ]
+
+    let a = document.querySelector('img')
+
+    let num = 0;
+    setInterval(function(){
+        a.setAttribute('src',arr[num]);
+        num = (num+1)%arr.length;
+    },2000)
+
---
+++ lect24/queryselector.js
@@ -0,0 +1,27 @@
+// let h1= document.querySelector('h1')
+// console.log(h1);
+
+
+// let h2= document.querySelectorAll('h1')
+// console.log(h2);
+
+// THIS INNERTEXT CHANGES THE TEXT INSIDE THE TAG AND CAN BE MANIPULATED
+
+// let h1= document.querySelector('h1')
+// h1.innerText='byeeeee'
+// console.log(h1,'ihifhfhsifhsdi');
+
+// ----------------------------------------------------------------------------------------------------------------
+
+// let a = document.querySelector('a')
+
+// a.setAttribute('href','http://youtube.com')             // setAttribute sets the value of the selected attribute(key)
+
+// console.log(a.getAttribute('href'));                    // getAttribute gets the value of the selected attribute
+
+// -----------------------------------------------------------------------------------------------
+
+let div =document.querySelector('div')
+let h1 = document.createElement('h1')
+h1.innerText= 'HKAklaojdosaijsojdl'
+div.appendChild(h1)
